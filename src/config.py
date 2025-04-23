1f1ab501e9983d3761139808ed806a1810e25a3e
Saumya B <60267060+Saumya4321@users.noreply.github.com>
2025-04-23 10:54:36 +0530

    Delete src directory

--- src/config.py
+++
@@ -1,2 +0,0 @@
-class Config:
-    DEVELOPMENT_MODE = False  # Set to False in production
--- src/moonrakerClient/__init__.py
+++
@@ -1,3 +0,0 @@
-# File: /pyqt-3d-printer-app/pyqt-3d-printer-app/src/octoprint_client/__init__.py
-
-# This file is intentionally left blank.
